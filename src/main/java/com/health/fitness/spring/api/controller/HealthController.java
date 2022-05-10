7329a44237fa74f34a01bb1aaf46ef246a83dd4c
donkageorgieva <DoniGeorgieva16@gmail.com>
2022-05-10 15:33:56 +0300

    Get Stats from Request Body

--- src/main/java/com/health/fitness/spring/api/controller/HealthController.java
+++ src/main/java/com/health/fitness/spring/api/controller/HealthController.java
@@ -1,16 +1,19 @@
 package com.health.fitness.spring.api.controller;
 
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import healt.fitness.api.model.Metrics;
 
 @RestController
 public class HealthController {
 
 	@PostMapping("/bmi")
-	public double calculateBMI(@RequestParam("kg") double kg, @RequestParam("cm") double cm) {
-		double meters = cm / 100;
-		double bmi = kg / (meters * meters);
+	public Double calculateBMI(@RequestBody Metrics metrics) {
+		double meters = metrics.height / 100;
+		double bmi = metrics.weight / (meters * meters);
 		return bmi;
 	}
 }
