ba30acdfa8d241519ffe9b855ab689b8e8f2c0df
donkageorgieva <DoniGeorgieva16@gmail.com>
2022-05-10 12:43:35 +0300

    Fix BMI controller

--- src/main/java/com/health/fitness/spring/api/controller/HealthController.java
+++ src/main/java/com/health/fitness/spring/api/controller/HealthController.java
@@ -1,11 +1,13 @@
 package com.health.fitness.spring.api.controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HealthController {
 
-	public double getBMI(double kg, double cm) {
+	@PostMapping("/bmi")
+	public double calculateBMI(double kg, double cm) {
 		double meters = cm / 100;
 		double bmi = kg / (meters * meters);
 		return bmi;
