b266bcc1d6bea9e845b255e0455c1d4e33d224c8
donkageorgieva <DoniGeorgieva16@gmail.com>
2022-05-10 15:02:16 +0300

    Metrics Models

--- src/main/java/com/health/fitness/spring/api/controller/HealthController.java
+++ src/main/java/com/health/fitness/spring/api/controller/HealthController.java
@@ -1,13 +1,14 @@
 package com.health.fitness.spring.api.controller;
 
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HealthController {
 
 	@PostMapping("/bmi")
-	public double calculateBMI(double kg, double cm) {
+	public double calculateBMI(@RequestParam("kg") double kg, @RequestParam("cm") double cm) {
 		double meters = cm / 100;
 		double bmi = kg / (meters * meters);
 		return bmi;
---
+++ src/main/java/healt/fitness/api/model/Metrics.java
@@ -0,0 +1,6 @@
+package healt.fitness.api.model;
+
+public class Metrics {
+private Double height;
+private Double weight;
+}
