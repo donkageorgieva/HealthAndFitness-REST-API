007ad3fce8f99d034e1dce36299382d997512a7a
donkageorgieva <DoniGeorgieva16@gmail.com>
2022-05-10 10:07:28 +0300

    Init

---
+++ src/main/java/com/health/fitness/spring/api/HealthAndFitnessApiApplication.java
@@ -0,0 +1,13 @@
+package com.health.fitness.spring.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthAndFitnessApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthAndFitnessApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/health/fitness/spring/api/HealthAndFitnessApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.health.fitness.spring.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthAndFitnessApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
